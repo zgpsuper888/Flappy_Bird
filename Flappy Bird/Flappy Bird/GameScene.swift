30b647de48f29ed1f7adc93a6d4566cec9af24be
peter <gaopan.zhang@duodian.com>
2017-03-09 15:49:26 +0800

    增加障碍物

--- Flappy Bird/Flappy Bird/GameScene.swift
+++ Flappy Bird/Flappy Bird/GameScene.swift
@@ -14,6 +14,7 @@ import AVFoundation
 
 enum 图层:CGFloat{
      case 背景
+     case 障碍物层
      case 前景
      case 游戏角色
 }
@@ -26,6 +27,10 @@ class GameScene: SKScene {
     
     let k重力:CGFloat = -1500.0
     let  k上冲速度:CGFloat = 400.0
+    
+    let k底部障碍物最小乘数:CGFloat=0.1
+    
+    let k底部障碍物最大乘数:CGFloat=0.1
     
     var 速度 = CGPoint.zero
     
@@ -47,11 +52,11 @@ class GameScene: SKScene {
         let url = Bundle.main.url(forResource: "flapping", withExtension: "wav")!
         
         do {
-            player = try AVAudioPlayer(contentsOf: url)
+            player = try AVAudioPlayer(contentsOf: url);
             guard let player = player else { return }
             
-            player.prepareToPlay()
-            player.play()
+            player.prepareToPlay();
+            player.play();
         } catch let error {
             print(error.localizedDescription)
         }
@@ -66,6 +71,8 @@ class GameScene: SKScene {
         设置前景();
         
         设置主角();
+        
+        生成障碍物();
         
         
     }
@@ -104,7 +111,7 @@ class GameScene: SKScene {
         前景.anchorPoint = CGPoint(x: 0, y: 1.0);
         前景.position = CGPoint(x: CGFloat(i) * 前景.size.width, y: 游戏区域起始点);
         前景.zPosition=图层.前景.rawValue;
-        前景.name="前景"
+        前景.name="前景";
         世界单位.addChild(前景);
         
         }
@@ -116,19 +123,42 @@ class GameScene: SKScene {
             
         }
     
+    func 创建障碍物(图片名:String )->SKSpriteNode{
+        let 障碍物=SKSpriteNode(imageNamed: 图片名);
+        障碍物.zPosition=图层.障碍物层.rawValue;
+        return 障碍物;
+
+    
+    }
+    
+    func 生成障碍物(){
+        let 底部障碍=创建障碍物(图片名: "CactusBottom")
+        let 起始X坐标=size.width/2
+        let y坐标最小值=(游戏区域起始点 - 底部障碍.size.height/2) + 游戏区域的高度 * k底部障碍物最小乘数
+        let y坐标最大值=(游戏区域起始点 - 底部障碍.size.height/2) + 游戏区域的高度 * k底部障碍物最大乘数
+
+        let y坐标=CGFloat(Float(arc4random()) / Float(UINT32_MAX)) * (y坐标最大值 - y坐标最小值) + y坐标最小值;
+
+        底部障碍.position=CGPoint(x:起始X坐标,y:y坐标)
+        世界单位.addChild(底部障碍)
+        
+        
+    }
+    
+    
        func  主角飞一下(){
-        速度 = CGPoint(x:0,y:k上冲速度)
-        播放主角飞一下声音()
+        速度 = CGPoint(x:0,y:k上冲速度);
+        播放主角飞一下声音();
        }
     
         override func update( _ 当前时间: TimeInterval) {
             if 上一次更新时间>0{
-                dt = 当前时间 - 上一次更新时间
+                dt = 当前时间 - 上一次更新时间;
             }else{
             
               dt=0
             }
-            上一次更新时间=当前时间
+            上一次更新时间=当前时间;
             
             更新主角();
             更新前景();
@@ -153,6 +183,12 @@ class GameScene: SKScene {
             if let 前景 = 匹配单位 as? SKSpriteNode {
             let 地面移动速度 = CGPoint(x: self.k地面移动速度, y: 0)
             前景.position += 地面移动速度 * CGFloat(self.dt)
+                if 前景.position.x < -前景.size.width{
+                    前景.position += CGPoint(x:前景.size.width*CGFloat(self.k前景地面数),y:0)
+                
+                }
+            
+            
             }
         }
     }
